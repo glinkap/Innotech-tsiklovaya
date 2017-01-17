743b10878eaced4edcb1cab4412b48dc1e7d641b
Petr Glinka <glinkap@gmail.com>
2017-01-17 16:26:49 +0300

    2

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+ 
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
+	sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./assets/sass/**/*.scss')
+	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))    
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
